d5c22f4fd0fde2deaf796e8f355816079fcc371f
Shontae Hogan <shontaehogan@gmail.com>
2017-09-05 20:56:28 -0400

    added requires and files

---
+++ starter_files/blackjack.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require_relative 'bet'
+require_relative 'cards'
+require_relative 'deal'
+require_relative 'deck'
+require_relative 'game'
+require_relative 'player'
+
+module Blackjack
+  #  code goes here
+end
